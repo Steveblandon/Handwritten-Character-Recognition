c8358547f9334c2091aeb49c9c26a6063e511ed1
Steve Blandon <steveblandon@gmail.com>
2016-03-24 16:18:51 -0400

    fixed images matrix

--- MNISTDecoder.m
+++ MNISTDecoder.m
@@ -9,7 +9,6 @@ count = fread(fid, 1, 'int32');
 labels = fread(fid, count, 'char');
 fclose(fid);
 
-
 fid = fopen(ImagesFile,'r','b');
 if (fread(fid, 1, 'int32') ~= 2051)
   disp('wrong images file')
@@ -19,11 +18,14 @@ if (fread(fid, 1, 'int32') ~= count)
   disp('images file is incompatible with labels file, check sizes')
   return
 end
-
 w = fread(fid, 1, 'int32');
 h = fread(fid, 1, 'int32');
 images = fread(fid, inf, 'uint8');
-images = reshape(images, [count w*h]); 
-#uncomment line below to rescale pixel values to be between 0 and 1
-#images = double(images) / 255;
 fclose(fid);
+
+#note: reshape() works column by column
+images = reshape(images, [w*h count]);
+images = images';
+
+#uncomment tabbed line below to rescale pixel values to be between 0 and 1
+images = double(images) / 255; 
